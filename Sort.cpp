5e03852893ff9b15e4eb6e38ac187336f8510043
ivan mamani <46487626+ivaniuss@users.noreply.github.com>
2019-10-03 17:06:27 -0500

    Update Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -53,69 +53,40 @@ void merge_sort(It left, It right)
   
 }
 
-/*
-template <typename T> size_t MedianOfThree(T &a, T &b, T &c, size_t ai, size_t bi, size_t ci)
+
+using namespace std;
+template<typename T>
+int partition(T *a,int start,int end)
 {
-	if (a >= b && a <= c)
-		return ai;
-	if (b >= a && b <= c)
-		return bi;
-	return ci;
+    int pivot=a[end];
+    int P_index=start;
+    int i,t;
+
+    for(i=start;i<end;i++)
+    {
+        if(a[i]<=pivot)
+        {
+            t=a[i];
+            a[i]=a[P_index];
+            a[P_index]=t;
+            P_index++;
+        }
+    }
+    t=a[end];
+    a[end]=a[P_index];
+    a[P_index]=t;
+
+    return P_index;
 }
 
-template <typename T> size_t QuicksortPartition(vector<T> &A, size_t lo, size_t hi)
+template<typename T>
+void Quicksort(T *a,int start,int end)
 {
-	size_t ci = (int)(0.5*(lo+hi)+0.5);
-	size_t pivotIndex = MedianOfThree(A[lo], A[ci], A[hi], lo, ci, hi);
-	T pivot = A[pivotIndex];
-
-	size_t i = lo;
-	size_t j = hi;
-	while(true)
-	{
-		while(A[i] <= pivot && i < pivotIndex) i++;
-		while(A[j] >= pivot && j > pivotIndex) j--;
-		if (i == pivotIndex && j == pivotIndex)
-			return pivotIndex;
-
-		T tmp = A[i];
-		A[i] = A[j];
-		A[j] = tmp;
-
-		if(i == pivotIndex)
-			pivotIndex = j;
-		else if(j == pivotIndex)
-			pivotIndex = i;
-	}
+    if(start<end)
+    {
+        int P_index=partition(a,start,end);
+        Quicksort(a,start,P_index-1);
+        Quicksort(a,P_index+1,end);
+    }
 }
 
-template <typename T> void Quicksort(vector<T> &A, size_t lo, size_t hi)
-{
-	if (lo < hi)
-	{
-		size_t p = QuicksortPartition<T>(A, lo, hi);
-		if (p > 0) 
-			Quicksort<T>(A, lo, p-1);
-		Quicksort<T>(A, p+1, hi);
-	}
-}
-
-template <typename T> void Quicksort(vector<T> &A)
-{
-	if (A.size() <= 1)
-		return; 
-	Quicksort(A, 0, A.size()-1);
-}
-
-// ******************************************
-
-template <typename T> bool SortResultIsValid(const vector<T> &A)
-{
-	for(size_t i = 1; i < A.size(); i++)
-		if (A[i] < A[i-1])
-			return false;
-	return true;
-}
-
-
-*/
