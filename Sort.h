f1862ce55bdc99ec7044b4a7ed7ef047e7aa49de
ivan mamani <46487626+ivaniuss@users.noreply.github.com>
2019-10-03 17:09:15 -0500

    Update Sort.h

--- Sort.h
+++ Sort.h
@@ -13,44 +13,26 @@
 template <typename T>
 
 class Sort{
-    std::vector<T> values;
-    void mergeSort( int p, int r);
-    void quickSort( int p, int r);
-    int partition( int p, int r);
+ 
 public:
 //interfaces
-    Sort(std::vector<T> values){
-        this->values = values;
-    }
-
+   std::vector<T> values;
+    explicit Sort(std::vector<T> values);
     void MergeSort(){
-        mergeSort(0,values.size()-1);
+        merge_sort(0,values.size()-1);
     }
-
     void QuickSort(){
-        quickSort(0, values.size()-1);
+        quickSort(0,values.size()-1);
     }
+    void mergeSort( int , int );
+    void quickSort( int , int );
+    int partition( int , int );
+    void HeapSort();
+    void ShellSort();
+    void print();
+   std::vector<T> values;
 
-
-    void print(){
-        for (auto & i:values)
-            std::cout<<i<<" ";
-        std::cout<<std::endl;
-    }
-};
-
-//libreria artesanal para que funcionen los templates
-
-template class Sort<int>;
-template class Sort<unsigned int>;
-template class Sort<long int>;
-template class Sort<long long int>;
-template class Sort<unsigned long int>;
-template class Sort<unsigned long long int>;
-template class Sort<char>;
-template class Sort<std::string>;
-template class Sort<double>;
-template class Sort<float>;
+;
 
 
 #endif //SORT_METHODS_SORT_H
